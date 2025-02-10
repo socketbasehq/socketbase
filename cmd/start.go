1e8be0febbbfc76563fb7076fcd558d85b753ce9
mxvsh <xencodes@icloud.com>
2025-02-10 15:21:45 +0530

    feat: add database auto-migration for App and User models
    - Added auto-migration for App and User models using GORM
    - Imported models package in start.go
    - Configured database auto-migration during application startup

--- cmd/start.go
+++ cmd/start.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/socketbasehq/socketbase/pkg/db"
+	"github.com/socketbasehq/socketbase/pkg/models"
 	"github.com/socketbasehq/socketbase/pkg/modules"
 	"github.com/socketbasehq/socketbase/pkg/server"
 	"github.com/spf13/cobra"
@@ -14,6 +15,9 @@ var startCmd = &cobra.Command{
 	Short: "Start the server",
 	Run: func(cmd *cobra.Command, args []string) {
 		db.ConnectDB()
+
+		db.DB.AutoMigrate(&models.App{})
+		db.DB.AutoMigrate(&models.User{})
 
 		app := fx.New(
 			modules.Modules,
